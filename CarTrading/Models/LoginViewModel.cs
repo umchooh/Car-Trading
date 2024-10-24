0b34ef26055a2a5fa03fc213ed107c33eb5ce74e
JmRubi0 <johnmatthewrubio0530@gmail.com>
2024-10-24 13:33:56 -0400

    Login-SignUP MVC

---
+++ CarTrading/Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace CarTrading.Models
+{
+    public class LoginViewModel
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String ErrorMessage { get; set; }
+    }
+}
