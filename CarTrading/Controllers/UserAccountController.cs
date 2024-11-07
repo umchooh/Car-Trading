5dccc71aca03d3ae991c2eda9438d319a3f1e022
Mark-Rubio <rubiomark817@gmail.com>
2024-11-06 20:50:53 -0500

    UserAccountController

---
+++ CarTrading/Controllers/UserAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarTrading.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
